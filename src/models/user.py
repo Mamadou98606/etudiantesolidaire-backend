feea0ca7de152052c04bf456dfad10ce2f6baea9
Mamadou98606 <78264446+Mamadou98606@users.noreply.github.com>
2025-08-13 11:52:40 +0200

    Update user.py

--- src/models/user.py
+++ src/models/user.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from src.database.db import db
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
